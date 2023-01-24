cc57c9e28ebcbdee021cedf931d9fe46a20945b2
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2023-01-24 22:39:28 +0530

    Update TestChargingCurrentReadings.cpp

--- TestChargingCurrentReadings.cpp
+++ TestChargingCurrentReadings.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <unistd.h>
-#include "test/catch.hpp"
+#include "catch.hpp"
 #include "typewise-alert.h"
 
 TEST_CASE("infers the charging current range") {
